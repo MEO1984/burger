7e75348fc5e31478510f7bc737e46234bfcf9bc9
Mike O'Malley <MEO1984@gmail.com>
2020-01-28 16:03:51 -0500

    selectAll still not working

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burger", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burger", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burger", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burger", condition, cb, function(res) {
+            cb(res);
+        });
+    }
+
+};
+module.exports = burger;
